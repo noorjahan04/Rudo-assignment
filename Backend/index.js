f2e9a32111eac34ab73f66ed5bc7d4e0d5a1e0f3
Noor jahan <noorifms@gmail.com>
2026-01-08 18:20:26 +0530

    assignment

--- Backend/index.js
+++ Backend/index.js
@@ -13,7 +13,9 @@ const debtSimplificationRoutes = require('./routes/debtSimplificationRoutes');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI =
-  process.env.MONGODB_URI 
+  process.env.MONGODB_URI ||
+  "mongodb+srv://noorifms:ZmYDE2bwIv3RJfJE@cluster0.wrkw41t.mongodb.net/city?retryWrites=true&w=majority&appName=Cluster0";
+
 // Middleware
 app.use(cors());
 app.use(express.json());
